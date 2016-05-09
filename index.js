7a7f373c87fea67c2181d337b32e1ffd36a0c048
Jose Barrios <jlbarrios22@gmail.com>
2016-05-09 03:52:20 -0700

    API 2.0 Release: now using REST, throttled request upgrades

--- index.js
+++ index.js
@@ -1,94 +1,164 @@
 ////////////////////////////////////////////////////
 //                   Imports
 ////////////////////////////////////////////////////
-var EventEmitter = require("events").EventEmitter;
+var PromiseThrottle = require('promise-throttle');
+var parseXML = require('xml2js').parseString;
+var querystring = require('querystring');
 var CryptoJS = require('crypto-js');
 var Promise = require('promise');
-var soap = require('soap');
+const https = require('https');
 
-//var WSDL = 'https://mechanicalturk.amazonaws.com/AWSMechanicalTurk/AWSMechanicalTurkRequester.wsdl';
-var WSDL = __dirname + '/schemas/AWSMechanicalTurkRequester-2014-08-15.wsdl';
-var PRODUCTION = 'https://mechanicalturk.amazonaws.com/';
-var SANDBOX = 'https://mechanicalturk.sandbox.amazonaws.com/';
+var PRODUCTION = 'mechanicalturk.amazonaws.com';
+var SANDBOX = 'mechanicalturk.sandbox.amazonaws.com';
 var SERVICE = 'AWSMechanicalTurkRequester';
+var VERSION = '2014-08-15';
+var METHOD = 'POST';
+
+var specialResponses = {};
+specialResponses['CreateQualificationType'] = 'QualificationType';
+specialResponses['CreateHIT'] = 'HIT';
+specialResponses['GetHIT'] = 'HIT';
+
+var promiseThrottle = new PromiseThrottle({
+    requestsPerSecond: 3,           // up to 1 request per second
+    promiseImplementation: Promise  // the Promise library you are using
+});
+
 
 
 function MTurkAPI() {
 
     var api = this;
-    api.connect = function(options) {
-        return new Promise(function (resolve, reject) {
-            soap.createClient(WSDL, function(err, client) {
-                if(err){ reject(err) }
-                var endPoint = options.sandbox? SANDBOX : PRODUCTION;
-                client.setEndpoint(endPoint);
-                var operations = Object.keys(client.AWSMechanicalTurkRequester.AWSMechanicalTurkRequesterPort);
-                var numOperations = operations.length;
-                var processedOps = 0;
-                var wrapper = {};
 
-                operations.forEach(function(operation){
-                    wrapClientMethods(client, wrapper, options, operation);
-                    processedOps++;
-                    if(processedOps === numOperations){ resolve(wrapper) }
-                })
+    api.createClient = function(options) {
 
-                wrapper.validOps = function(){ return operations; }
-                wrapper.req = function(opName, args) {
-                    return new Promise(function(resolve, reject){
-                        if(wrapper[opName] && typeof wrapper[opName] == 'function') { resolve(wrapper[opName](args)); }
-                        else { reject("Invalid Amazon Mechanical Turk API operation: '"+opName+"'. See the docs here: https://goo.gl/6RCpKU"); }
-                    })
-                };
+        var client = {};
+        // add some items to the queue
+        client.req = function (operation, params) {
+            return new Promise(function(resolve, reject){
+                promiseThrottle.add(throttledRequest.bind(this, client, options, operation, params))
+                .then(resolve)
+                .catch(reject);
             })
-        })
-    };
+        };
+
+        client.isValidResponse = function(response){
+            var result = {};
+            result.errorType =  [];
+            result.errorMessage = null;
+            result.isValid = null;
+
+            var responseKey = Object.keys(response)[0];
+            var charIndex = responseKey.indexOf("Response");
+            var operationName = responseKey.slice(0, charIndex)
+
+            //Check for operation request errors
+            var responseProperty = operationName + 'Response';
+            var hasResponse = response.hasOwnProperty(responseProperty);
+            var responseGroup = hasResponse? response[operationName + 'Response'] : null;
+            var operationRequest = responseGroup? responseGroup.OperationRequest[0] : null;
+            var operationErrors = operationRequest? operationRequest.hasOwnProperty('Errors') : null;
+            var operationError = operationErrors? true : false;
+            var error = operationError? operationRequest.Errors[0].Error[0]: null;
+            var errCode = error? error.Code[0] : '';
+            var errMsg = error? error.Message[0] : '';
+            var errKey = error && error.hasOwnProperty('Data')? error.Data[0].Key : '';
+            var errVal = error && error.hasOwnProperty('Data')? error.Data[0].Value : '';
+            var errData = errKey && errVal? errKey +': '+errVal : '';
+            result.errorMessage = error? errCode +' - '+ errMsg + ' - '+ errData: '';
+            result.isValid = error? false : true;
+            if(error){return result}
+
+
+            //Check for result errors
+            var specialCase = specialResponses.hasOwnProperty(operationName);
+            var resultProperty = specialCase ? specialResponses[operationName] : operationName + 'Result';
+            var hasResult = responseGroup.hasOwnProperty(resultProperty);
+            var requestResult = hasResult? responseGroup[resultProperty][0].Request[0] : null;
+            var hasValidity = requestResult? requestResult.hasOwnProperty('IsValid'): null;
+            var validResult = hasValidity? requestResult.IsValid[0] === 'True': false;
+            var resultError = hasResult && !validResult;
+            var error = resultError? requestResult.Errors[0].Error[0]: null;
+            var errCode = error? error.Code[0] : '';
+            var errMsg = error? error.Message[0] : '';
+            var errKey = error && error.hasOwnProperty('Data')? error.Data[0].Key : '';
+            var errVal = error && error.hasOwnProperty('Data')? error.Data[0].Value : '';
+            var errData = errKey && errVal? errKey +': '+errVal : '';
+            result.errorMessage = error? errCode +' - '+ errMsg + ' - '+ errData : '';
+            result.isValid = error? false : true;
+            return result;
+        }
+
+        return client;
+    }
 
     return api;
 }
 
 
-///////////////////////////
-//   IMPLEMENTATION
-///////////////////////////
-function getRequestMessage(options, operation, parameters){
-    var message = {};
-    message.Request= parameters;
-    message.AWSAccessKeyId=  options.access;
-    message.Timestamp = new Date().toISOString();
-    var hmac = CryptoJS.algo.HMAC.create(CryptoJS.algo.SHA1, options.secret);
-    hmac.update(SERVICE + operation + message.Timestamp);
-    message.Signature = hmac.finalize().toString(CryptoJS.enc.Base64);
-    return message;
-}
 
-function wrapClientMethods(client, wrapper, options, operation){
-    wrapper[operation] = function(params){
-        var params = params || {};
-        return new Promise(function(resolve, reject){
-            if(typeof client[operation] === 'undefined'){return reject('Invalid operation: '+operation)}
+function throttledRequest(client, options, operation, params){
+    var params = params || {};
+    params.RequestGroup = params.RequestGroup || 'Request, Minimal';
+    params.Operation = operation;
+    params.Version = VERSION;
 
-            client[operation](getRequestMessage(options, operation, params), function(err, response){
-                if(err){ return reject(err) }
-                var keys = Object.keys(response);
-                var responseResult = keys[0];
-                var requestMade = response[responseResult];
-                if(keys[1]){
-                    responseResult = keys[1];
-                    requestMade = response[responseResult][0].Request;
-                }
-                //Note that we're not comparing to a Boolean, but to a String (which is part of the mturk response)
-                if(requestMade.IsValid === "True"){
-                    resolve(response);
-                }
-                else { //Catch response errors:
-                    var message = operation+" - "+requestMade.Errors.Error[0].Message;
-                    var error = new Error(message);
-                    reject(error);
-                }
-            });
+    return new Promise(function(resolve, reject){
+        var signedRequest = signRequest(options, params);
+        signedRequest = querystring.stringify(signedRequest);
+        var payloadSize = Buffer.byteLength(signedRequest);
+
+        var reqOptions = {};
+        reqOptions.host = options.sandbox? SANDBOX : PRODUCTION;
+        reqOptions.method= METHOD;
+        reqOptions.headers = {};
+        reqOptions.headers['Content-Type'] = 'application/x-www-form-urlencoded';
+        reqOptions.headers['Content-Length'] = payloadSize;
+
+        var req = https.request(reqOptions, function(res) {
+
+            if(res.statusCode !== 200){
+                var error = new Error(res.statusCode +' - '+ res.statusMessage)
+                reject(error);
+            }
+
+            var xmlBuffer = new Buffer([]);
+            res.on('data', function(chunk) {
+                xmlBuffer = Buffer.concat([xmlBuffer, chunk]);
+            })
+
+            res.on('end', function(){
+                var xmlString = xmlBuffer.toString('utf-8');
+                convertXMLToJSON(xmlString).then(function(JSONResponse){
+                    var test = client.isValidResponse(JSONResponse);
+                    test.isValid? resolve(JSONResponse) : reject(new Error(test.errorMessage))
+                }).catch(reject);
+            })
+
+            res.on('error', reject)
         });
-    }
+
+        req.write(signedRequest);
+        req.end();
+    })
+};
+
+
+function convertXMLToJSON(xml){
+    return new Promise(function(resolve, reject){
+        parseXML(xml, function (err, response) {
+            err? reject(err) : resolve(response);
+        });
+    })
+};
+
+function signRequest(credentials, params){
+    params.AWSAccessKeyId=  credentials.access;
+    params.Timestamp = new Date().toISOString();
+    var hmac = CryptoJS.algo.HMAC.create(CryptoJS.algo.SHA1, credentials.secret);
+    hmac.update(SERVICE + params.Operation + params.Timestamp);
+    params.Signature = hmac.finalize().toString(CryptoJS.enc.Base64);
+    return params;
 }
 
 //EXPORT
--- test/test.js
+++ test/test.js
@@ -2,79 +2,79 @@ var mturk = require('../index.js');
 var config = require('../config.js');
 var maxTimeout = "10000";
 config.sandbox = true;
+var validator = require('../validateMTurkResponse.js');
 
-var API = {};
+var api = mturk.createClient(config);
 
-
-function randomString(size) {
-    var random = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    for( var i=0; i < size; i++ ){
-        random += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-    return random;
-}
+//These variables are assign as we move through the tests
+var workerId = "A2Q1RSC9MWUTL2";
+var QualificationTypeId = null;
+var HITId = null;
+var HITTypeId = null;
+var assignmentId = null;
 
 
 describe('Amazon Mechanical Turk API', function() {
 
-    it('Connection', function(done) {
+    it('SearchHITs', function(done) {
         this.timeout(maxTimeout)
-        mturk.connect(config).then(function(client) {
-            API = client;
-            API.WorkerId = "A2Q1RSC9MWUTL2";
-            API.HITId = '3G57RS03HHDCF84CT9872BY0GAT52N';
+        var params = {PageSize: 1}
+        api.req('SearchHITs', params).then(function(res){
+            HITId = res.SearchHITsResponse.SearchHITsResult[0].HIT[0].HITId[0];
+            done();
+        }).catch(done);
+    });
+
+    it('CreateQualificationType', function(done) {
+        this.timeout(maxTimeout)
+        var params = { Name:'SANDBOX_QUALIFICATION', Description:'THIS IS A SANDBOX QUALIFICATION FOR TESTING PURPOSES', QualificationTypeStatus:'Active' }
+        api.req('CreateQualificationType', params).then(function(res){
+            qualificationTypeId = res.CreateQualificationTypeResponse.QualificationType[0].QualificationTypeId[0];
             done();
         }).catch(done);
     });
 
 
+
     it('AssignQualification', function(done) {
         this.timeout(maxTimeout)
-        var params = { Name:'Random qualification '+randomString(10), Description:'Description', QualificationTypeStatus:'Active' }
-        API.req('CreateQualificationType', params).then(function(res){
-            API.QualificationTypeId = res.QualificationType[0].QualificationTypeId;
-            API.req('AssignQualification', { QualificationTypeId:API.QualificationTypeId, WorkerId:API.WorkerId }).then(function(res){
-                done();
-                API.req('DisposeQualificationType', {QualificationTypeId:API.QualificationTypeId} )
-            }).catch(done);
+        api.req('AssignQualification', { QualificationTypeId: qualificationTypeId, WorkerId:workerId }).then(function(res){
+            done();
         }).catch(done);
     });
 
 
+    it('DisposeQualificationType', function(done){
+        api.req('DisposeQualificationType',{QualificationTypeId : qualificationTypeId}).then(function(res){
+            done();
+        }).catch(done);
+    })
+
+
     it('GetAccountBalance', function(done) {
         this.timeout(maxTimeout)
-        API.req('GetAccountBalance').then(function(res) {
+        api.req('GetAccountBalance').then(function(res) {
             done();
         }).catch(done);
     });
 
     it('BlockWorker', function(done) {
         this.timeout(maxTimeout)
-        API.req('BlockWorker', {WorkerId:API.WorkerId, Reason:"Testing block operation" }).then(function(res){
+        api.req('BlockWorker', {WorkerId:workerId, Reason:"Testing block operation" }).then(function(res){
             done();
         }).catch(done);
     });
 
     it('GetBlockedWorkers', function(done) {
         this.timeout(maxTimeout)
-        API.req('GetBlockedWorkers').then(function(res){
+        api.req('GetBlockedWorkers').then(function(res){
             done();
         }).catch(done);
     });
 
     it('UnblockWorker', function(done) {
         this.timeout(maxTimeout)
-        API.req('UnblockWorker', { WorkerId:API.WorkerId }).then(function(res){
-            done();
-        }).catch(done);
-    });
-
-
-
-    it('SearchHITs', function(done) {
-        this.timeout(maxTimeout)
-        API.req('SearchHITs').then(function(res) {
+        api.req('UnblockWorker', { WorkerId:workerId }).then(function(res){
             done();
         }).catch(done);
     });
@@ -82,7 +82,7 @@ describe('Amazon Mechanical Turk API', function() {
 
     it('GetHIT', function(done) {
         this.timeout(maxTimeout)
-        API.req('GetHIT', { HITId:API.HITId } ).then(function(res){
+        api.req('GetHIT', { HITId:HITId } ).then(function(res){
             done();
         }).catch(done);
     });
@@ -90,28 +90,29 @@ describe('Amazon Mechanical Turk API', function() {
 
     it('ForceExpireHIT', function(done) {
         this.timeout(maxTimeout)
-        API.req('ForceExpireHIT', { HITId:API.HITId }).then(function(res){
+        api.req('ForceExpireHIT', { HITId:HITId }).then(function(res){
             done();
         }).catch(done);
     });
 
-    it('SendTestEventNotification', function(done) {
 
-        this.timeout(maxTimeout)
-        var notification = {
-            Destination: 'z8k5o8o3q0i1n9q8@cognilab.slack.com',
-            Transport: "Email",
-            Version: "2006-05-05",
-            EventType: ["Ping"]
-        }
-        var params = {
-            Notification: notification,
-            TestEventType: ["Ping"]
-        }
-        API.req('SendTestEventNotification', params).then(function(res){
-            done();
-        }).catch(done);
-    });
+    /*    it('SendTestEventNotification', function(done) {*/
+
+    //this.timeout(maxTimeout)
+    //var notification = {
+    //Destination: 'z8k5o8o3q0i1n9q8@cognilab.slack.com',
+    //Transport: "Email",
+    //Version: "2006-05-05",
+    //EventType: ["Ping"]
+    //}
+    //var params = {
+    //Notification: notification,
+    //TestEventType: ["Ping"]
+    //}
+    //api.req('SendTestEventNotification', params).then(function(res){
+    //done();
+    //}).catch(done);
+    //});
 
 
 
@@ -127,58 +128,58 @@ describe('Amazon Mechanical Turk API', function() {
 
 //var pipe = {};
 //UNTESTED:
-//API.req('GetQualificationScore', { QualificationTypeId:pipe.QualificationTypeId, SubjectId:pipe.WorkerId} ).then(handleResponse, handleError);
-//API.req('GrantQualification', { QualificationRequestId:String }).then(handleResponse, handleError);
-//API.req('RejectQualificationRequest', { QualificationRequestId:String } ).then(handleResponse, handleError);
-//API.req('UpdateQualificationScore', { QualificationTypeId:String, SubjectId:String, IntegerValue:Number } ).then(handleResponse, handleError);
-//API.req('ApproveAssignment', { AssignmentId:String } ).then(handleResponse, handleError);
+//api.req('GetQualificationScore', { QualificationTypeId:pipe.QualificationTypeId, SubjectId:pipe.WorkerId} ).then(handleResponse, handleError);
+//api.req('GrantQualification', { QualificationRequestId:String }).then(handleResponse, handleError);
+//api.req('RejectQualificationRequest', { QualificationRequestId:String } ).then(handleResponse, handleError);
+//api.req('UpdateQualificationScore', { QualificationTypeId:String, SubjectId:String, IntegerValue:Number } ).then(handleResponse, handleError);
+//api.req('ApproveAssignment', { AssignmentId:String } ).then(handleResponse, handleError);
 
 
-//API.req('SearchHITs').then(function(res){
+//api.req('SearchHITs').then(function(res){
 //pipe.HITId = res.HIT[0].HITId;
-//API.req('ExtendHIT', { HITId:pipe.HITId, ExpirationIncrementInSeconds:6000 } ).then(handleResponse, handleError);
-//API.req('DisableHIT', { HITId:pipe.HITId } ).then(handleResponse, handleError);
-//API.req('DisposeHIT', { HITId:pipe.HITId } ).then(handleResponse, handleError);
+//api.req('ExtendHIT', { HITId:pipe.HITId, ExpirationIncrementInSeconds:6000 } ).then(handleResponse, handleError);
+//api.req('DisableHIT', { HITId:pipe.HITId } ).then(handleResponse, handleError);
+//api.req('DisposeHIT', { HITId:pipe.HITId } ).then(handleResponse, handleError);
 //}, handleError);
 
 //VALID
 //pipe.QualificationTypeId = response.QualificationTypeId;
-//API.req('UpdateQualificationType',  { QualificationTypeId:pipe.QualificationTypeId, Description: 'New Description' }).then(handleResponse, handleError);
-//API.req('GetHITsForQualificationType', { QualificationTypeId:pipe.QualificationTypeId } ).then(handleResponse, handleError);
-//API.req('GetQualificationsForQualificationType', { QualificationTypeId:pipe.QualificationTypeId } ).then(handleResponse, handleError);
-//API.req('GetQualificationRequests').then(handleResponse, handleError);
-//API.req('GetQualificationType', { QualificationTypeId:pipe.QualificationTypeId } ).then(handleResponse, handleError);
+//api.req('UpdateQualificationType',  { QualificationTypeId:pipe.QualificationTypeId, Description: 'New Description' }).then(handleResponse, handleError);
+//api.req('GetHITsForQualificationType', { QualificationTypeId:pipe.QualificationTypeId } ).then(handleResponse, handleError);
+//api.req('GetQualificationsForQualificationType', { QualificationTypeId:pipe.QualificationTypeId } ).then(handleResponse, handleError);
+//api.req('GetQualificationRequests').then(handleResponse, handleError);
+//api.req('GetQualificationType', { QualificationTypeId:pipe.QualificationTypeId } ).then(handleResponse, handleError);
 //setTimeout(function(){
-////API.req('RevokeQualification', { SubjectId:pipe.WorkerId, QualificationTypeId:pipe.QualificationTypeId, Reason:'Whatever' } ).then(handleResponse, handleError);
+////api.req('RevokeQualification', { SubjectId:pipe.WorkerId, QualificationTypeId:pipe.QualificationTypeId, Reason:'Whatever' } ).then(handleResponse, handleError);
 //}, 8000);
 //}, handleError);
 
-/*    API.req('BlockWorker', { WorkerId:pipe.WorkerId, Reason:'Reason goes here' } ).then(function(res){*/
-//API.req('GetBlockedWorkers').then(handleResponse, handleError);
-//API.req('UnblockWorker',  { WorkerId:pipe.WorkerId }).then(handleResponse, handleError);
+/*    api.req('BlockWorker', { WorkerId:pipe.WorkerId, Reason:'Reason goes here' } ).then(function(res){*/
+//api.req('GetBlockedWorkers').then(handleResponse, handleError);
+//api.req('UnblockWorker',  { WorkerId:pipe.WorkerId }).then(handleResponse, handleError);
 //}, handleError);
 
-//API.req('SearchQualificationTypes', { MustBeRequestable:true } ).then(function(res){
+//api.req('SearchQualificationTypes', { MustBeRequestable:true } ).then(function(res){
 //});
 
 
-//API.req('ChangeHITTypeOfHIT', { HITId:String, HITTypeId:String} ).then(handleResponse, handleError);
-//API.req('CreateHIT', { Title:String, Description:String, AssignmentDurationInSeconds:Number, LifetimeInSeconds:Number } ).then(handleResponse, handleError);
-//API.req('GetAssignment', { AssignmentId:String } ).then(handleResponse, handleError);
-//API.req('GetAssignmentsForHIT', { HITId:String } ).then(handleResponse, handleError);
-//API.req('GetBonusPayments', { HITId:String } ).then(handleResponse, handleError);
-//API.req('GetFileUploadURL', { AssignmentId:String, QuestionIdentifier:String } ).then(handleResponse, handleError);
-//API.req('GetRequesterStatistic', { Statistic:String, TimePeriod:String } ).then(handleResponse, handleError);
-//API.req('GetRequesterWorkerStatistic', {Statistic:String, WorkerId:String, TimePeriod:String } ).then(handleResponse, handleError);
-//API.req('GetReviewableHITs').then(handleResponse, handleError);
-//API.req('GetReviewResultsForHIT', { HITId:String } ).then(handleResponse, handleError);
-//API.req('GrantBonus', { WorkerId:String, AssignmentId:String, BonusAmount:Object, Reason:String }).then(handleResponse, handleError);
-//API.req('NotifyWorkers', { Subject:String, MessageText:String, WorkerId:Array} ).then(handleResponse, handleError);
-//API.req('RegisterHITType', { Title:String, Description:String, Reward:Object, AssignmentDurationInSeconds:Number } ).then(handleResponse, handleError);
-//API.req('RejectAssignment', { AssignmentId:String } ).then(handleResponse, handleError);
-//API.req('SendTestEventNotification', { Notification:Object, TestEventType:Object } ).then(handleResponse, handleError);
-//API.req('SetHITAsReviewing', { HITId:String } ).then(handleResponse, handleError);
-//API.req('SetHITTypeNotification', { HITTypeId:String, Notification:Object } ).then(handleResponse, handleError);
+//api.req('ChangeHITTypeOfHIT', { HITId:String, HITTypeId:String} ).then(handleResponse, handleError);
+//api.req('CreateHIT', { Title:String, Description:String, AssignmentDurationInSeconds:Number, LifetimeInSeconds:Number } ).then(handleResponse, handleError);
+//api.req('GetAssignment', { AssignmentId:String } ).then(handleResponse, handleError);
+//api.req('GetAssignmentsForHIT', { HITId:String } ).then(handleResponse, handleError);
+//api.req('GetBonusPayments', { HITId:String } ).then(handleResponse, handleError);
+//api.req('GetFileUploadURL', { AssignmentId:String, QuestionIdentifier:String } ).then(handleResponse, handleError);
+//api.req('GetRequesterStatistic', { Statistic:String, TimePeriod:String } ).then(handleResponse, handleError);
+//api.req('GetRequesterWorkerStatistic', {Statistic:String, WorkerId:String, TimePeriod:String } ).then(handleResponse, handleError);
+//api.req('GetReviewableHITs').then(handleResponse, handleError);
+//api.req('GetReviewResultsForHIT', { HITId:String } ).then(handleResponse, handleError);
+//api.req('GrantBonus', { WorkerId:String, AssignmentId:String, BonusAmount:Object, Reason:String }).then(handleResponse, handleError);
+//api.req('NotifyWorkers', { Subject:String, MessageText:String, WorkerId:Array} ).then(handleResponse, handleError);
+//api.req('RegisterHITType', { Title:String, Description:String, Reward:Object, AssignmentDurationInSeconds:Number } ).then(handleResponse, handleError);
+//api.req('RejectAssignment', { AssignmentId:String } ).then(handleResponse, handleError);
+//api.req('SendTestEventNotification', { Notification:Object, TestEventType:Object } ).then(handleResponse, handleError);
+//api.req('SetHITAsReviewing', { HITId:String } ).then(handleResponse, handleError);
+//api.req('SetHITTypeNotification', { HITTypeId:String, Notification:Object } ).then(handleResponse, handleError);
 
 //}, function(connError){
 ////Handle connection error here

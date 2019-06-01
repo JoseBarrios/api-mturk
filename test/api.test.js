77b56c7f83afb9458bee1ac50a2b0d5cb4006235
Jose Barrios <github@barrios.io>
2019-05-31 17:03:20 -0700

    Improved legacy tests

--- test/api.test.js
+++ test/api.test.js
@@ -10,83 +10,83 @@ const mturk = new MTurkAPI(config);
 
 describe("API Version 2017-01-17", function () {
 
-/*  it("GetAccountBalance", async function (done) {*/
-    //const res = await mturk.req("GetAccountBalance");
-//[>    expect(res).to.be.an('object');<]
-    ////expect(res.AvailableBalance).to.be.an('string');
-    ////done();
-
-    //assert.equal(res.AvailableBalance, "10000.00");
-    //const balanceFunc = await mturk.getAccountBalance();
-    //assert.equal(balanceFunc, 10000.00);
-    //const balanceProp = await mturk.accountBalance;
-    //assert.equal(balanceProp, 10000.00);
+  it("GetAccountBalance", async function (done) {
+    const res = await mturk.req("GetAccountBalance");
+    expect(res).to.be.an('object');
+    //expect(res.AvailableBalance).to.be.an('string');
     //done();
-  //});
 
-  //it("CreateHIT", function() {
-    //fs.readFile('./templates/HTMLQuestion.xml', 'utf8', async function(err, data){
-      //if(err){throw new Error(err)}
-      //var params = {
-        //Title: "EXAMPLE",
-        //Description: "Answer the questions on the screen",
-        //Question: data,
-        //Keywords: "test, HIT",
-        //AssignmentDurationInSeconds: 180, // in 3 minutes
-        //AutoApprovalDelayInSeconds: 0, // auto approve the worker's anwser and pay to him/her
-        //MaxAssignments: 1, // 100 worker
-        //LifetimeInSeconds: 86400 * 3, // 3 days
-        //Reward: "0.01"
-      //};
+    assert.equal(res.AvailableBalance, "10000.00");
+    const balanceFunc = await mturk.getAccountBalance();
+    assert.equal(balanceFunc, 10000.00);
+    const balanceProp = await mturk.accountBalance;
+    assert.equal(balanceProp, 10000.00);
+    done();
+  });
 
-      //const res = await mturk.req('CreateHIT', params);
-      //expect(res).to.be.an('object');
-      //expect(res.HIT).to.be.an('object');
-      //expect(res.HIT.AssignmentDurationInSeconds).to.be.a('number');
-      //expect(res.HIT.AutoApprovalDelayInSeconds).to.be.a('number');
-      //expect(res.HIT.CreationTime).to.be.a('number');
-      //expect(res.HIT.Description).to.be.a('string');
-      //expect(res.HIT.Expiration).to.be.a('number');
-      //expect(res.HIT.HITGroupId).to.be.a('string');
-      //expect(res.HIT.HITId).to.be.a('string');
-      //expect(res.HIT.HITReviewStatus).to.be.a('string');
-      //expect(res.HIT.HITStatus).to.be.a('string');
-      //expect(res.HIT.HITTypeId).to.be.a('string');
-      //expect(res.HIT.Keywords).to.be.a('string');
-      //expect(res.HIT.MaxAssignments).to.be.a('number');
-      //expect(res.HIT.NumberOfAssignmentsAvailable).to.be.a('number');
-      //expect(res.HIT.NumberOfAssignmentsCompleted).to.be.a('number');
-      //expect(res.HIT.NumberOfAssignmentsPending).to.be.a('number');
-      //expect(res.HIT.QualificationRequirements).to.be.an('array');
-      //expect(res.HIT.Question).to.be.a('string');
-      //expect(res.HIT.Reward).to.be.a('string');
-      //expect(res.HIT.Title).to.be.a('string');
-    //});
-  //});
+  it("CreateHIT", function() {
+    fs.readFile('./templates/HTMLQuestion.xml', 'utf8', async function(err, data){
+      if(err){throw new Error(err)}
+      var params = {
+        Title: "EXAMPLE",
+        Description: "Answer the questions on the screen",
+        Question: data,
+        Keywords: "test, HIT",
+        AssignmentDurationInSeconds: 180, // in 3 minutes
+        AutoApprovalDelayInSeconds: 0, // auto approve the worker's anwser and pay to him/her
+        MaxAssignments: 1, // 100 worker
+        LifetimeInSeconds: 86400 * 3, // 3 days
+        Reward: "0.01"
+      };
 
-  //it('ListHITs', async function() {
-    //const res = await mturk.req('ListHITs');
-    ////console.log(JSON.stringify(res, null, " "))
-    //expect(res).to.be.an('object');
-    //expect(res.HITs).to.be.an('array');
-    //expect(res.HITs).to.be.an('array');
-    //expect(res.HITs[0].HITId).to.be.a('string');
-    //expect(res.HITs[0].HITTypeId).to.be.a('string');
-    //expect(res.HITs[0].HITGroupId).to.be.a('string');
-    //expect(res.HITs[0].CreationTime).to.be.a('number');
-    //expect(res.HITs[0].Title).to.be.a('string');
-    //expect(res.HITs[0].Description).to.be.a('string');
-    //expect(res.HITs[0].HITStatus).to.be.a('string');
-    //expect(res.HITs[0].MaxAssignments).to.be.a('number');
-    //expect(res.HITs[0].Reward).to.be.an('string');
-    //expect(res.HITs[0].AutoApprovalDelayInSeconds).to.be.an('number');
-    //expect(res.HITs[0].Expiration).to.be.a('number');
-    //expect(res.HITs[0].AssignmentDurationInSeconds).to.be.an('number');
-    //expect(res.HITs[0].HITReviewStatus).to.be.an('string');
-    //expect(res.HITs[0].NumberOfAssignmentsPending).to.be.an('number');
-    //expect(res.HITs[0].NumberOfAssignmentsAvailable).to.be.an('number');
-    //expect(res.HITs[0].NumberOfAssignmentsCompleted).to.be.an('number');
-  //});
+      const res = await mturk.req('CreateHIT', params);
+      expect(res).to.be.an('object');
+      expect(res.HIT).to.be.an('object');
+      expect(res.HIT.AssignmentDurationInSeconds).to.be.a('number');
+      expect(res.HIT.AutoApprovalDelayInSeconds).to.be.a('number');
+      expect(res.HIT.CreationTime).to.be.a('number');
+      expect(res.HIT.Description).to.be.a('string');
+      expect(res.HIT.Expiration).to.be.a('number');
+      expect(res.HIT.HITGroupId).to.be.a('string');
+      expect(res.HIT.HITId).to.be.a('string');
+      expect(res.HIT.HITReviewStatus).to.be.a('string');
+      expect(res.HIT.HITStatus).to.be.a('string');
+      expect(res.HIT.HITTypeId).to.be.a('string');
+      expect(res.HIT.Keywords).to.be.a('string');
+      expect(res.HIT.MaxAssignments).to.be.a('number');
+      expect(res.HIT.NumberOfAssignmentsAvailable).to.be.a('number');
+      expect(res.HIT.NumberOfAssignmentsCompleted).to.be.a('number');
+      expect(res.HIT.NumberOfAssignmentsPending).to.be.a('number');
+      expect(res.HIT.QualificationRequirements).to.be.an('array');
+      expect(res.HIT.Question).to.be.a('string');
+      expect(res.HIT.Reward).to.be.a('string');
+      expect(res.HIT.Title).to.be.a('string');
+    });
+  });
+
+  it('ListHITs', async function() {
+    const res = await mturk.req('ListHITs');
+    //console.log(JSON.stringify(res, null, " "))
+    expect(res).to.be.an('object');
+    expect(res.HITs).to.be.an('array');
+    expect(res.HITs).to.be.an('array');
+    expect(res.HITs[0].HITId).to.be.a('string');
+    expect(res.HITs[0].HITTypeId).to.be.a('string');
+    expect(res.HITs[0].HITGroupId).to.be.a('string');
+    expect(res.HITs[0].CreationTime).to.be.a('number');
+    expect(res.HITs[0].Title).to.be.a('string');
+    expect(res.HITs[0].Description).to.be.a('string');
+    expect(res.HITs[0].HITStatus).to.be.a('string');
+    expect(res.HITs[0].MaxAssignments).to.be.a('number');
+    expect(res.HITs[0].Reward).to.be.an('string');
+    expect(res.HITs[0].AutoApprovalDelayInSeconds).to.be.an('number');
+    expect(res.HITs[0].Expiration).to.be.a('number');
+    expect(res.HITs[0].AssignmentDurationInSeconds).to.be.an('number');
+    expect(res.HITs[0].HITReviewStatus).to.be.an('string');
+    expect(res.HITs[0].NumberOfAssignmentsPending).to.be.an('number');
+    expect(res.HITs[0].NumberOfAssignmentsAvailable).to.be.an('number');
+    expect(res.HITs[0].NumberOfAssignmentsCompleted).to.be.an('number');
+  });
 
 
 });
